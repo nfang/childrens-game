a324ebad69d94dd04d4941bbcb48ee1bdcdc44fe
Nathan Fang <nyyrikkifang@gmail.com>
2017-06-30 16:04:29 +1000

    :wrench: add commands to clean up docker containers/images before docker-compose build

--- build.sh
+++ build.sh
@@ -6,4 +6,13 @@ dotnet publish ChildrensGame.Api -c Release -o ./dist
 
 dotnet publish ChildrensGame.Cli -c Release -o ./dist
 
+docker stop -t 15 childrensgame_container_api
+docker stop -t 15 childrensgame_container_cli
+
+docker rm childrensgame_container_api
+docker rm childrensgame_container_cli
+
+docker rmi childrensgame_api
+docker rmi childrensgame_cli
+
 docker-compose build
